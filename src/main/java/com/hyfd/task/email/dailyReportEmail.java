acff0cf99af931ffc4bb8b1450b7f1f29511715d
wangsheng <ws18363348428@163.com>
2021-03-05 15:20:54 +0800

    //添加财报数据发送日志打印

--- src/main/java/com/hyfd/task/email/dailyReportEmail.java
+++ src/main/java/com/hyfd/task/email/dailyReportEmail.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -20,11 +21,14 @@ import com.hyfd.common.utils.MailUtil;
 import com.hyfd.dao.mp.multipleReportDao;
 
 import net.sf.jxls.transformer.XLSTransformer;
+import sun.rmi.runtime.Log;
+
+import javax.xml.crypto.Data;
 
 @Component
 public class dailyReportEmail {
 
-	Logger log = Logger.getLogger(getClass());
+	static Logger log = Logger.getLogger(dailyReportEmail.class);
 
 	@Autowired
 	multipleReportDao mRDao;
@@ -42,11 +46,15 @@ public class dailyReportEmail {
 	 */
 	@Scheduled(cron = "0 30 4 * * ? ")
 	public void sendDailyReportEmail(){
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String startformat = simpleDateFormat.format(new Date());
+		log.info(startformat + "财报准备数据开始----------------------------------------");
 		String date = DateTimeUtils.formatDate(DateTimeUtils.addDays(new Date(), -1),"yyyy年MM月dd日");
 //		String date = "2018年02月26日";
 //		String dailyFilePath = path.replaceAll("/WEB-INF/classes/", "") + File.separator + "temp"+ File.separator+date+".xlsx";
 		String dailyFilePath = path.replaceAll("/WEB-INF/classes/", "") + "/" + "temp"+"/"+date+".xlsx";
 		createDailyExcel(dailyFilePath);
+		log.info("财报数据模板完成准备发送-------------------------------------");
 		Map<String,Object> mail = new HashMap<String,Object>();
 		mail.put("tos", tos);
 		mail.put("copyto", copyto);
@@ -55,8 +63,13 @@ public class dailyReportEmail {
 		mail.put("fileAddr", dailyFilePath);
 		mail.put("fileName", date+"数据.xlsx");
 		try {
+			log.info("财报数据模板完成准备发送-------------------------------------");
 			MailUtil.sendExcel(mail);
+			String endformat = simpleDateFormat.format(new Date());
+			log.info(endformat + "发送财报数据成功----------------------------------");
 		} catch (Exception e) {
+			String errformat = simpleDateFormat.format(new Date());
+			log.error(errformat + "发送财报数据发生异常--------------------------:",e);
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -76,7 +89,7 @@ public class dailyReportEmail {
 		map.put("title", "每日数据");
 		map.put("agentDataList", agentList);
 		map.put("providerDataList", providerList);
-		
+		log.info("获取每日发送的财报数据"+ map + "-----------------------------------------------------");
 		exportExcel("dailyReportTemp",filePath,map);
 		
  	}
@@ -95,7 +108,9 @@ public class dailyReportEmail {
 	        OutputStream os = new FileOutputStream(file);
 	        workbook.write(os);
 	        os.close();
+			log.info("把数据写到模板成功--------------------------------");
         }catch(Exception e){
+			log.info("把数据写到模板出现异常--------------------------------");
         	e.printStackTrace();
         }
 	}
