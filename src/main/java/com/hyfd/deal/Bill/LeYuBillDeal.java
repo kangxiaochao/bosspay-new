913b313bc20a605598962d69fb18ea2d753004f0
wangsheng <ws18363348428@163.com>
2022-01-14 11:41:19 +0800

    leyu接口对接

---
+++ src/main/java/com/hyfd/deal/Bill/LeYuBillDeal.java
@@ -0,0 +1,156 @@
+package com.hyfd.deal.Bill;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hyfd.common.utils.*;
+import com.hyfd.deal.BaseDeal;
+import org.apache.log4j.Logger;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class LeYuBillDeal implements BaseDeal {
+
+    public static Map<String, String> lyMap = new HashMap<String, String>();
+
+    static {
+        lyMap.put("10001", "appKey不能为空");
+        lyMap.put("10002", "请求类型不能为空");
+        lyMap.put("10003", "交易流水不能为空");
+        lyMap.put("10004", "时间戳不能为空");
+        lyMap.put("10005", "请求内容不能为空");
+        lyMap.put("10006", "签名不能为空");
+        lyMap.put("10007", "appKey不存在");
+        lyMap.put("10008", "签名验证失败");
+        lyMap.put("10009", "充值号码不能为空");
+        lyMap.put("10010", "充值金额或实付金额不能小于0");
+        lyMap.put("10011", "用户非在网或非本网，请检查充值号码");
+        lyMap.put("10012", "交易流水已存在");
+        lyMap.put("10013", "渠道账户扣费异常");
+        lyMap.put("10014", "查询号码不能为空");
+        lyMap.put("10015", "号码格式错误");
+        lyMap.put("10016", "缴费单号不能为空");
+    }
+
+
+    private static Logger log = Logger.getLogger(LeYuBillDeal.class);
+
+    @Override
+    public Map<String, Object> deal(Map<String, Object> order) {
+        log.info("乐语order" + order);
+        Map<String, Object> map = new HashMap<String, Object>();
+        int flag = -1;
+        try {
+            String phoneNo = (String) order.get("phone");    // 手机号
+            Double fee = (Double) order.get("fee");//金额，以厘为单位
+            Double ibillsize = Double.parseDouble(fee + "") * 1000;
+            String spec = ibillsize.intValue() + "";
+
+            Map<String, Object> channel = (Map<String, Object>) order.get("channel");    // 获取通道参数
+            String linkUrl = channel.get("link_url").toString();                        // 充值地址
+            String defaultParameter = (String) channel.get("default_parameter");//默认参数
+            Map<String, String> paramMap = XmlUtils.readXmlToMap(defaultParameter.trim());
+            String appKey = paramMap.get("AppKey") + "";        // 乐语提供 平台appKay
+            String appSecret = paramMap.get("AppSecret") + "";    // 乐语提供 平台appSecret
+
+
+            String curids = ToolDateTime.format(new Date(), "yyyyMMddHHmmssSSS") + phoneNo + GenerateData.getIntData(9, 2);
+            map.put("orderId", curids);
+            String result = rechargeOrder(linkUrl, appKey, appSecret, phoneNo, spec, curids);
+//            {"msg":"success","code":0,"responseMsg":{"rechargeId":"LYTXRC202201121530035860917"}}
+
+            if (null == result || result.equals("")) {
+                // 请求超时,未获取到返回数据
+                String msg = "乐语话费充值,号码[" + phoneNo + "],金额[" + spec + "(元)],请求超时,未接收到返回数据";
+                map.put("resultCode", msg);
+                log.error(msg);
+            }else {
+                JSONObject jsonObj = JSONObject.parseObject(result);
+                JSONObject responseMsg = jsonObj.getJSONObject("responseMsg");
+                String code = jsonObj.getString("code");
+                String msg = jsonObj.getString("msg");
+
+                if (code.equals("0")){
+                    String rechargeId = responseMsg.getString("rechargeId");
+                    map.put("providerOrderId", rechargeId);
+                    flag = 1;	// 提交成功
+                    map.put("resultCode",msg);
+                }else {
+                    flag = 0;	// 提交失败
+                    map.put("resultCode",lyMap.get(code));
+                }
+
+            }
+
+        } catch (Exception e) {
+            log.error("乐语话费充值出错" + e.getMessage() + MapUtils.toString(order));
+        }
+
+
+        map.put("status", flag);
+        return map;
+    }
+
+
+    /**
+     * <h5>功能:充值请求</h5>
+     *
+     * @param linkUrl   请求地址
+     * @param appkey    平台appKey
+     * @param appsecret 平台appSecret
+     * @param phoneNo   待充值的用户号码
+     * @param spec      充值金额,单位:厘
+     * @param curids    订单号
+     * @return
+     * @author zhangpj    @date 2018年11月13日
+     */
+    public String rechargeOrder(String linkUrl, String appkey, String appsecret, String phoneNo, String spec, String curids) {
+        String nowTime = DateUtils.getNowTimeToSec();
+
+        Map mapMsg = new HashMap();
+        mapMsg.put("serviceNum", phoneNo);
+        mapMsg.put("amount", spec);
+        String redultjson = JSONObject.toJSONString(mapMsg);
+
+        SortedMap map = new TreeMap();
+        map.put("requestType", "RECHARGE");
+        map.put("transactionId", curids);
+        map.put("timeStamp", nowTime);
+        map.put("appKey", appkey);
+        map.put("requestMsg", redultjson);
+
+        String sign = createSign(map, appsecret);
+        map.put("sign", sign);
+        String redultjsons = JSONObject.toJSONString(map);
+        log.info("乐语充值请求参数:" + redultjsons);
+        String data = ToolHttp.post(false, linkUrl, redultjsons, "application/json");
+        log.info("乐语充值请求响应结果:" + data);
+        return redultjsons;
+    }
+
+
+    public static String createSign(SortedMap<Object, Object> params, String APP_SECRET) {
+        StringBuffer sb = new StringBuffer();
+        Set es = params.entrySet();
+        Iterator it = es.iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String k = (String) entry.getKey();
+            String v = (String) entry.getValue();
+            if (null != v && !"".equals(v) && !"sign".equals(k) && !"key".equals(k)) {
+                sb.append(k + "=" + v + "&");
+            }
+        }
+        sb.append("key=" + APP_SECRET);
+        System.out.println("sb = " + sb);
+        String sign = null;
+        try {
+            sign = MD5Util.MD5Encode(sb.toString(), "UTF-8");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sign;
+    }
+}
---
+++ src/main/java/com/hyfd/task/LeYuTask.java
@@ -0,0 +1,145 @@
+package com.hyfd.task;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hyfd.common.utils.DateUtils;
+import com.hyfd.common.utils.MD5Util;
+import com.hyfd.common.utils.ToolHttp;
+import com.hyfd.common.utils.XmlUtils;
+import com.hyfd.dao.mp.OrderDao;
+import com.hyfd.dao.mp.ProviderPhysicalChannelDao;
+import com.hyfd.rabbitMq.RabbitMqProducer;
+import com.hyfd.rabbitMq.SerializeUtil;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class LeYuTask {
+
+
+    @Autowired
+    ProviderPhysicalChannelDao providerPhysicalChannelDao;// 物理通道信息
+
+    @Autowired
+    OrderDao orderDao;// 订单
+
+    @Autowired
+    RabbitMqProducer mqProducer;// 消息队列生产者
+
+
+    private static Logger log = Logger.getLogger(LeYuTask.class);
+
+
+    @Scheduled(fixedDelay = 60000)
+    public void queryLeYuOrder() {
+
+        Map<String, Object> resultmap = new HashMap<String, Object>();
+
+        try {
+            String id = "2000000071";// 乐语物理通道ID ~~~~~
+            Map<String, Object> channel = providerPhysicalChannelDao.selectByPrimaryKey(id);// 获取通道的数据
+            String defaultParameter = (String) channel.get("default_parameter");// 默认参数
+            Map<String, String> paramMap = XmlUtils.readXmlToMap(defaultParameter);
+            String ServerUrl = paramMap.get("ServerUrl") + ""; 		// 乐语提供 查单url
+            String appKey = paramMap.get("AppKey") + ""; 		// 乐语提供 平台appKay
+            String appSecret = paramMap.get("AppSecret") + ""; 	// 乐语提供 平台appSecret
+
+            Map<String, Object> param = new HashMap<String, Object>();
+            param.put("dispatcherProviderId", id);
+            param.put("status", "1");
+            List<Map<String, Object>> orderList = orderDao.selectByTask(param);
+
+            for (Map<String, Object> order : orderList) {
+                int flag = 2;
+
+                String orderId = order.get("orderId") + "";
+                String providerOrderId = order.get("providerOrderId") + "";
+                String nowTime = DateUtils.getNowTimeToSec();
+                resultmap.put("orderId", orderId);
+
+                Map mapParm  = new HashMap();
+                mapParm.put("rechargeId",providerOrderId);
+                String redultjson = JSONObject.toJSONString(mapParm);
+
+                SortedMap map = new TreeMap();
+                map.put("appKey",appKey);
+                map.put("requestType","QUERY");
+                map.put("timeStamp", nowTime);
+                map.put("requestMsg",redultjson);
+                String sign = createSign(map, appSecret);
+
+                map.put("sign",sign);
+                map.put("transactionId","");
+                String redultjsons = JSONObject.toJSONString(map);
+                log.info("乐语查单请求参数:"+redultjsons);
+                String data = ToolHttp.post(false, ServerUrl, redultjsons,"application/json");
+                log.info("乐语查单响应结果:" + data);
+//                {"msg":"success","code":0,"responseMsg":{"serviceNum":"17001000000","remark":"充值成功","orderPay":1000,"transactionId":"0a7b0039b6af45ab888c9e7f7066ded9","rechargeId":"LYTXRC202201121530035860917","status":1,"createDate":"2022-01-12 15:30:04","completeDate":"2022-01-12 15:30:12"}}
+                if (null != data && !data.equals("")) {
+                    JSONObject jsonObj = JSONObject.parseObject(data);
+                    String code = jsonObj.getString("code");
+                    if (code.equals("0")){
+                        JSONObject responseMsg = jsonObj.getJSONObject("responseMsg");
+                        String status = responseMsg.getString("status");
+                        String remark = responseMsg.getString("remark");
+                        resultmap.put("resultCode", code + ":" + remark);
+                        if (status.equals("1")){
+                            flag = 1;	  //充值成功
+                        }else if(status.equals("2")) {
+                            flag = 0;     //充值失败
+                        }else if (status.equals("0")){
+                            continue;    //充值订单处理中
+                        }
+                        resultmap.put("status", flag);
+                        mqProducer.sendDataToQueue(RabbitMqProducer.Result_QueueKey, SerializeUtil.getStrFromObj(resultmap));
+                    }else {
+                        //查单请求出现异常
+                        log.error("乐语请求订单出现异常："+order);
+                    }
+
+
+                }
+
+            }
+
+        }catch (Exception e){
+            log.error("乐语Task出错" + e);
+        }
+    }
+
+
+    /**
+     * @param params 请求参数
+     * @return
+     * @author
+     * @date 2016-4-22
+     * @Description：sign签名
+     */
+    public static String createSign(SortedMap<Object, Object> params, String APP_SECRET) {
+        StringBuffer sb = new StringBuffer();
+        Set es = params.entrySet();
+        Iterator it = es.iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String k = (String) entry.getKey();
+            String v = (String) entry.getValue();
+            if (null != v && !"".equals(v) && !"sign".equals(k) && !"key".equals(k)) {
+                sb.append(k + "=" + v + "&");
+            }
+        }
+        sb.append("key=" + APP_SECRET);
+        System.out.println("sb = " + sb);
+        String sign = null;
+        try {
+            sign = MD5Util.MD5Encode(sb.toString(),"UTF-8");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return sign;
+    }
+}
