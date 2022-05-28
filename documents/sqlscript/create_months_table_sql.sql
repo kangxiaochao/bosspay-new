0b3414dd06fee4bc0b77d7115e77b9da65e17f75
Administrator <Administrator@192.168.1.122>
2022-05-28 22:13:51 +0800

    mp_agent_account_charge、mp_order、mp_provider_account_charge、mp_submit_order、mp_order_all_agent分表完成，sql建表脚本

---
+++ documents/sqlscript/create_months_table_sql.sql
@@ -0,0 +1,45 @@
+-- mp_agent_acount_charge--
+CREATE TABLE mp_agent_account_charge_202205 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202206 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202207 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202208 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202209 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202210 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202211 LIKE mp_agent_account_charge;
+CREATE TABLE mp_agent_account_charge_202212 LIKE mp_agent_account_charge;
+-- mp_order --
+CREATE TABLE mp_order_202205 LIKE mp_order;
+CREATE TABLE mp_order_202206 LIKE mp_order;
+CREATE TABLE mp_order_202207 LIKE mp_order;
+CREATE TABLE mp_order_202208 LIKE mp_order;
+CREATE TABLE mp_order_202209 LIKE mp_order;
+CREATE TABLE mp_order_202210 LIKE mp_order;
+CREATE TABLE mp_order_202211 LIKE mp_order;
+CREATE TABLE mp_order_202212 LIKE mp_order;
+-- mp_mp_provider_account_charge --
+CREATE TABLE mp_provider_account_charge_202205 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202206 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202207 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202208 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202209 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202210 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202211 LIKE mp_provider_account_charge;
+CREATE TABLE mp_provider_account_charge_202212 LIKE mp_provider_account_charge;
+-- mp_submit_order --
+CREATE TABLE mp_submit_order_202205 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202206 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202207 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202208 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202209 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202210 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202211 LIKE mp_submit_order;
+CREATE TABLE mp_submit_order_202212 LIKE mp_submit_order;
+-- mp_order_all_agent --
+CREATE TABLE mp_order_all_agent_202205 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202206 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202207 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202208 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202209 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202210 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202211 LIKE mp_order_all_agent;
+CREATE TABLE mp_order_all_agent_202212 LIKE mp_order_all_agent;
