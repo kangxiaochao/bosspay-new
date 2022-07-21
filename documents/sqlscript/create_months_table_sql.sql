6dd9353755b2faad24b6e9df5556716c34bf23cf
Administrator <ksgj12040729>
2022-07-21 20:56:35 +0800

    provider_account_charge表改为provider_charge_record

--- documents/sqlscript/create_months_table_sql.sql
+++ documents/sqlscript/create_months_table_sql.sql
@@ -16,15 +16,15 @@ CREATE TABLE mp_order_202209 LIKE mp_order;
 CREATE TABLE mp_order_202210 LIKE mp_order;
 CREATE TABLE mp_order_202211 LIKE mp_order;
 CREATE TABLE mp_order_202212 LIKE mp_order;
--- mp_mp_provider_account_charge --
-CREATE TABLE mp_provider_account_charge_202205 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202206 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202207 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202208 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202209 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202210 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202211 LIKE mp_provider_account_charge;
-CREATE TABLE mp_provider_account_charge_202212 LIKE mp_provider_account_charge;
+-- mp_provider_charge_record --
+CREATE TABLE mp_provider_charge_record_202205 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202206 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202207 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202208 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202209 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202210 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202211 LIKE mp_provider_charge_record;
+CREATE TABLE mp_provider_charge_record_202212 LIKE mp_provider_charge_record;
 -- mp_submit_order --
 CREATE TABLE mp_submit_order_202205 LIKE mp_submit_order;
 CREATE TABLE mp_submit_order_202206 LIKE mp_submit_order;
